dc9d6118d1002e3fafcee7252708dede04f713b8
Yousef-Jammal <ywsfalsaydh@gmail.com>
2024-06-05 12:07:51 +0300

    Update main.js

--- tasks 6-4/secend array/main.js
+++ tasks 6-4/secend array/main.js
@@ -1,5 +1,84 @@
 // 1-Write a function to find the largest element in an array.
 
+// console.log("Try programiz.pro");
+
+// let number1 = [2, 3 , 4 , 5 , 6 , 7 , 88 , 11  , 32 , 47,-1];
+
+// while (number1.length > 1) {
+//   if (number1.length == 1) {
+//     break;
+//   } else if (number1[1] >= number1[0]) {
+//     number1.shift();
+//   } else if (number1[1] < number1[0]) {
+//     number1.splice(1, 1);
+//   }
+// }
+
+// console.log(number1);
+
+// 2-Write a function to find the smallest element in an array.
+
+// console.log("Try programiz.pro");
+
+// let number2 = [2, 3 , 4 , 5 , 6 , 7 , 88 , 11  , 32 , 47,-1];
+
+// while (number2.length > 1) {
+//   if (number2.length == 1) {
+//     break;
+//   } else if (number2[1] <= number2[0]) {
+//     number2.shift();
+//   } else if (number2[1] > number2[0]) {
+//     number2.splice(1, 1);
+//   }
+// }
+
+// console.log(number2);
+
+
+// 3-Write a function to find the sum of all elements in an array.
+// let number3 = [3, 4, 7, 9, 10 , 10];
+// let sum3 = 0;
+// for (let i = 0; i < number3.length ; i++){
+//     sum3 += number3[i]
+// }
+// console.log(sum3);
+// 4-Write a function to find the average of all elements in an array.
+
+// let number4 = [3, 4, 7, 9, 10 , 10];
+// let sum4 = 0;
+// for (let i = 0; i < number4.length ; i++){
+//     sum4 += number4[i]
+// }
+// sum4 = sum4 / number4.length
+// console.log(sum4);
+
+// 5-Write a function to find the median of all elements in an array.
+// let number4 = [3, 4, 7, 9, 10 , 10];
+// let sum4 = 0;
+// for (let i = 0; i < number4.length ; i++){
+//     sum4 += number4[i]
+// }
+// // sum4 = sum4 / number4.length
+// console.log(sum4);
+// 6-Write a function to remove all duplicates from an array.
+// let number6 = [3, 4, 7, 9, 10 , 10];
+
+// for ( let i = 0; i < number6.length ; i++){
+//     console.log(i)
+//     for ( let x = 1; x < number6.length ; x++){
+//         console.log(x)
+//         if (number6[i] == number6[x]){
+//             number6.push([x]);
+//             number6.pop()
+//         }
+//         else{
+//             continue
+//         }
+//     }
+// }
+// console.log(number6)
+// 7-Write a function to sort an array in ascending order.
+
 // let number1 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 9];
 // console.log(number1.length);
 
@@ -24,39 +103,9 @@
 
 // console.log(number1);
 
-// 2-Write a function to find the smallest element in an array.
-
-// 3-Write a function to find the sum of all elements in an array.
-// let number3 = [3, 4, 7, 9, 10 , 10];
-// let sum3 = 0;
-// for (let i = 0; i < number3.length ; i++){
-//     // console.log(number3[i])
-//     sum3 += number3[i]
-// }
-// console.log(sum3);
-// 4-Write a function to find the average of all elements in an array.
-// let number4 = [3, 4, 7, 9, 10 , 10];
-// let sum4 = 0;
-// for (let i = 0; i < number4.length ; i++){
-//     sum4 += number4[i]
-// }
-// sum4 = sum4 / number4.length
-// console.log(sum4);
-// 5-Write a function to find the median of all elements in an array.
-// let number4 = [3, 4, 7, 9, 10 , 10];
-// let sum4 = 0;
-// for (let i = 0; i < number4.length ; i++){
-//     sum4 += number4[i]
-// }
-// // sum4 = sum4 / number4.length
-// console.log(sum4);
-// 6-Write a function to remove all duplicates from an array.
-// let number6 = [3, 4, 7, 9, 10 , 10];
-// for ( let i = 0; i < number6 ; i++){
-
-// }
-// 7-Write a function to sort an array in ascending order.
-
 // 8-Write a function to sort an array in descending order.
 
 // 9-Write a function to shuffle the elements of an array randomly.
+
+
+// presentation array and method in array
