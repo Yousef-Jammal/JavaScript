46b184153b6c8b85ab4d026dcc512427f484da9a
Yousef-Jammal <ywsfalsaydh@gmail.com>
2024-06-09 14:23:52 +0300

    Add files via upload

---
+++ task 6-6 DOM/main1.js
@@ -0,0 +1,7 @@
+let mydiv = document.getElementById("mydiv")
+mydiv.onmouseenter = function(){
+    mydiv.textContent = "hi their how are you"
+}
+mydiv.onmouseleave = function(){
+    mydiv.textContent = "Hello world"
+}
