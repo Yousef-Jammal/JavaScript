9b288e039cfd51d72ddb59fa4f6f9d59a12cbbf9
Yousef-Jammal <ywsfalsaydh@gmail.com>
2024-06-09 17:55:12 +0300

    Update main2.js

--- task 6-6 DOM/main2.js
+++ task 6-6 DOM/main2.js
@@ -1,9 +1,19 @@
 let element = document.getElementById("mydiv");
+let colors = 0;
 
 element.onclick = function () {
-  // let number = 0;
-  // number += 1;
-  // element.textContent = number;
-  // console.log(number);
-  element.style.backgroundColor = "red";
-  };
+  colors += 1;
+  if (colors == 1) {
+    element.style.backgroundColor = "red";
+  } else if (colors == 2) {
+    element.style.backgroundColor = "green";
+  } else if (colors == 3) {
+    element.style.backgroundColor = "blue";
+  } else if (colors == 4) {
+    element.style.backgroundColor = "black";
+  } else if (colors == 5) {
+    element.style.backgroundColor = "gold";
+  } else if (colors == 6) {
+    element.style.backgroundColor = "brown";
+  }
+};
